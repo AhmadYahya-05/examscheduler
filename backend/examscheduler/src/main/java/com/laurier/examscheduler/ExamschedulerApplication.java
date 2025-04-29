e2664a5858677018e628f683644ed9fb5fac834a
AhmadYahya-05 <ahmad.rafay.yahya@gmail.com>
2025-04-29 12:50:42 -0400

    Prepare for deployment - updated project and ignored env files

--- backend/examscheduler/src/main/java/com/laurier/examscheduler/ExamschedulerApplication.java
+++ backend/examscheduler/src/main/java/com/laurier/examscheduler/ExamschedulerApplication.java
@@ -1,12 +1,15 @@
 package com.laurier.examscheduler;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class ExamschedulerApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv.configure().load();
 		SpringApplication.run(ExamschedulerApplication.class, args);
 	}
 
