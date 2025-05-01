7ea871ecffaf3378ec352ab75ff4fc418dce0f61
AhmadYahya-05 <ahmad.rafay.yahya@gmail.com>
2025-05-01 00:28:39 -0400

    a

--- backend/examscheduler/src/main/java/com/laurier/examscheduler/controllers/HealthCheckController.java
+++ backend/examscheduler/src/main/java/com/laurier/examscheduler/controllers/HealthCheckController.java
@@ -1,11 +1,13 @@
 package com.laurier.examscheduler.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HealthCheckController {
+
     @GetMapping("/")
-    public String root() {
-        return "Laurier Exam Scheduler API is running and working!";
+    public String index() {
+        return "✅ Laurier Exam Scheduler Backend is Running!";
     }
-    
 }
